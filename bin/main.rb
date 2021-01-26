fe20f17252816483d35e134e7243e643830dbb82
AymenBida <aymen.bida.00@gmail.com>
2021-01-26 11:57:52 +0100

    Add instructions to use the app

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,33 @@
 #!/usr/bin/env ruby
 
 require_relative '../lib/bida_lib.rb'
+require 'colorize'
+
+languages = { en: 'English' }
+err = nil
+not_language = "Please choose one of the available languages by typing the language code ('en' for English)".bold.yellow
+
+if languages.length > 1
+  loop do
+    clear
+    puts '++ WELCOME DEAR USER ++'.bold.cyan
+    line_return
+    puts 'Please choose your language :'
+    line_return
+    languages.each { |code, lang| puts "#{code} - #{lang}" }
+    line_return
+    show_error(err, not_language)
+    choice = take_it.to_sym
+    if languages.keys.any?(choice)
+      require_relative "../lang/#{choice}.rb"
+      break
+    else
+      err = not_language
+    end
+  end
+else
+  require_relative "../lang/en.rb"
+end
+
+clear
+puts before_starting
--- lang/en.rb
+++ lang/en.rb
@@ -2,3 +2,18 @@
 # This way we can easily translate the app to other languages
 
 require 'colorize'
+
+def before_starting
+  "Make sure you follow this instructions to make use of this app:
+
+1 - Prepare your 'template.docx' file:
+
+  + Write your template (the text that is going to be there in all the cases)
+  + Add bookmarks with meaningful names (so you can understand them later) in places you want to fill
+
+    example: look at the 'example.docx' file
+
+2 - If you prepared everything correctly exit and run the 'bin/main.rb' from the terminal again and skip 1 and 2
+
+3 - Follow instructions to fill your template with text and save the results"
+end
